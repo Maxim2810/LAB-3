49db58257ceaa4709e81a175e039f7e9b14f59ba
Maxim2810 <ghostguy@bk.ru>
2019-05-12 22:30:16 +0700

    Dobavil test dlya poligon-test

--- test/poligon-test.c
+++ test/poligon-test.c
@@ -1,49 +1,26 @@
 #include "geometry.h"
-void printPoligon(
-        double x1,
-        double y1,
-        double x2,
-        double y2,
-        double x3,
-        double y3,
-        double x4,
-        double y4,
-        bool flag,
-        int k,
-        int lkl,
-        int n)
-{
-    double p, s, determinant, s1, s2;
-    p = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - x1))
-            + sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2))
-            + sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3))
-            + sqrt((x1 - x3) * (x1 - x4) + (y1 - y3) * (y1 - y4));
-    determinant = (x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3);
-    determinant < 0 ? s1 = determinant / (-2) : s1 = determinant / 2;
-    determinant = (x4 - x3) * (y2 - y3) - (x2 - y3) * (y1 - y3);
-    determinant < 0 ? s2 = determinant / (-2) : s2 = determinant / 2;
-    s = s1 + s2;
-    cout << n << endl;
-    printf("Poligon(%.1f %.1f , %.1f %.1f , %.1f %.1f , %.1f %.1f)\n",
-           x1,
-           y1,
-           x2,
-           y2,
-           x3,
-           y3,
-           x4,
-           y4);
-    printf("perimetr = %.3f\n", p);
-    printf("area = %.3f\n", s);
-    if (flag == 1) {
-        printf("intersects: \n   ");
-        if (lkl == 1) {
-            cout << k << ". Circle" << endl;
-        } else if (lkl == 2) {
-            cout << k << ". Triangle" << endl;
-        } else if (lkl == 3) {
-            cout << k << ". Poligon" << endl;
-        }
-    } else
-        printf("intersects: \n   - \n");
+int printPoligon(double x1, double y1, double x2, double y2, double x3,
+                 double y3, double x4, double y4, bool flag, int lkl) {
+  ;
+  if (flag == 1) {
+    if (lkl == 1) {
+      return 1;
+    } else if (lkl == 2) {
+      return 2;
+
+    } else if (lkl == 3) {
+      return 3;
+    }
+  } else
+    return 0;
+  return 0;
+}
+TEST_CASE("PRINT_POLIGON", "[POLIGON]") {
+  bool flag_ = false;
+  REQUIRE(printPoligon(1, 1, 1, 1, 1, 1, 1, 1, flag_, 1) == 0);
+  flag_ = true;
+  REQUIRE(printPoligon(1, 1, 1, 1, 1, 1, 1, 1, flag_, 1) == 1);
+  REQUIRE(printPoligon(1, 1, 1, 1, 1, 1, 1, 1, flag_, 2) == 2);
+  REQUIRE(printPoligon(1, 1, 1, 1, 1, 1, 1, 1, flag_, 3) == 3);
+  cout << "Test print poligon" << endl;
 }
