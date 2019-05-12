6785df61587d3c96adc59bd41f310e295fe6f28d
Maxim2810 <ghostguy@bk.ru>
2019-05-12 22:12:48 +0700

    Dobavil test dlya perpendicular-test

--- test/perpendicular-test.c
+++ test/perpendicular-test.c
@@ -3,3 +3,8 @@ void perpendicular(double a, double b, double c, double P, double& H)
 {
     H = (2 * sqrt(P * (P - a) * (P - b) * (P - c))) / c;
 }
+TEST_CASE("DLINA_PERPENDICULARA", "[90*]") {
+  REQUIRE(perpendicular(11, 11, 10, 16) == 9);
+  REQUIRE(perpendicular(6, 6, 9, 10.5) == 3);
+  cout << "Test perpendicular" << endl;
+}
