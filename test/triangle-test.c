4be846fea163fb2f4a9c822198ba4948c9c5af3b
Maxim2810 <ghostguy@bk.ru>
2019-05-12 22:30:58 +0700

    Dobavil test dlya triangle-test

--- test/triangle-test.c
+++ test/triangle-test.c
@@ -1,42 +1,27 @@
 #include "geometry.h"
-void printTriangle(
-        double x1,
-        double y1,
-        double x2,
-        double y2,
-        double x3,
-        double y3,
-        bool flag,
-        int k,
-        int lkl,
-        int n)
-{
-    double s, p, determinant;
-    p = sqrt((x2 - x1) * (x2 - x1)) + sqrt((x3 - x1) * (x3 - x1))
-            + sqrt((x3 - x2) * (x3 - x2));
-    determinant = (x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3);
+int printTriangle(double x1, double y1, double x2, double y2, double x3,
+                  double y3, bool flag, int lkl) {
+  if (flag == 1) {
+    if (lkl == 1) {
+      return 1;
 
-    determinant < 0 ? s = determinant / (-2) : s = determinant / 2;
-    cout << n << endl;
-    printf("Triangle(%.1f %.1f , %.1f %.1f , %.1f %.1f)\n",
-           x1,
-           y1,
-           x2,
-           y2,
-           x3,
-           y3);
-    printf("perimetr = %.3f\n", p);
-    printf("area = %.3f\n", s);
-    if (flag == 1) {
-        printf("intersects: \n   ");
-        if (lkl == 1) {
-            cout << k << ". Circle" << endl;
-        } else if (lkl == 2) {
-            cout << k << ". Triangle" << endl;
-        } else if (lkl == 3) {
-            cout << k << ". Poligon" << endl;
-        }
-    } else {
-        printf("intersects: \n   - \n");
+    } else if (lkl == 2) {
+      return 2;
+
+    } else if (lkl == 3) {
+      return 3;
     }
+  } else {
+    return 0;
+  }
+  return 0;
+}
+TEST_CASE("PRINT_TRIANGLE", "[TRIANGLE]") {
+  bool flag_ = false;
+  REQUIRE(printTriangle(1, 1, 1, 1, 1, 1, flag_, 1) == 0);
+  flag_ = true;
+  REQUIRE(printTriangle(1, 1, 1, 1, 1, 1, flag_, 1) == 1);
+  REQUIRE(printTriangle(1, 1, 1, 1, 1, 1, flag_, 2) == 2);
+  REQUIRE(printTriangle(1, 1, 1, 1, 1, 1, flag_, 3) == 3);
+  cout << "Test print Triagle" << endl;
 }
