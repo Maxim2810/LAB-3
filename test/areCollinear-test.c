a7cf2eca847e96467c8ab88a2c7eb6d51b94b18b
Maxim2810 <ghostguy@bk.ru>
2019-05-12 22:02:11 +0700

    Dobavil test dlya areCollinear

--- test/areCollinear-test.c
+++ test/areCollinear-test.c
@@ -2,3 +2,11 @@ bool areCollinear(double x1, double y1, double x2, double y2)
 {
     return x1 / x2 == y1 / y2 ? true : false;
 }
+
+TEST_CASE("ARECOLLINEAR", "[double]") {
+  REQUIRE(areCollinear(4, 12, 2, 6) == 1);
+  REQUIRE(areCollinear(1, 1, 1, 1) == 1);
+  REQUIRE(areCollinear(90, 12, 2, 6) == 0);
+  REQUIRE(areCollinear(123, 12, 2, 6) == 0);
+  cout << "Test areCollinear" << endl;
+}
